cf0e18f19871504a5f0ff186d42a8e6f6322ff00
Usman Akbar <45451919+usmanjoiya@users.noreply.github.com>
2023-05-03 15:32:13 +0500

    Update RealEstate.sol

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,17 +1,16 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
-
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+
 contract RealEstate is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+        constructor() ERC721("Real Estate", "REAL") {}
 
-    constructor() ERC721("Real Estate", "REAL") {}
-
-    function mint(string memory tokenURI) public returns (uint256) {
+function mint(string memory tokenURI) public returns (uint256) {
        _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
